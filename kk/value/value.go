3ef974768759c940d03fe5a00a0af81459d933ef
zhanghailong <zhanghailong@didichuxing.com>
2017-01-08 20:09:30 +0800

    fix bug

--- kk/value/value.go
+++ kk/value/value.go
@@ -393,6 +393,10 @@ func StringValue(value reflect.Value, defaultValue string) string {
 func SetValue(object reflect.Value, value reflect.Value) {
 
 	if value.Kind() == reflect.Invalid {
+		switch object.Kind() {
+		case reflect.Ptr:
+			object.Set(value)
+		}
 		return
 	}
 
